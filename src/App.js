23634a0e0b1a14d286c3396af2e6e5637b4f88a8
Alaa Desouky <alaa.desouky33@gmail.com>
2019-01-24 17:18:41 +0200

    building a reusable portal, modal, and icon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import Toggle from "./components/ToggleRPC";
+import Portal from "./components/Portal";
+import Modal from "./components/Modal";
 
 class App extends Component {
   render() {
@@ -9,12 +11,23 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <Toggle>
+          {/* <Toggle>
             {({ on, toggle }) => (
-              <div>
+              <>
                 {on && <h1>Hello Toggle from Render Props</h1>}
                 <button onClick={toggle}>Show/Hide</button>
-              </div>
+                <Portal>{on && <h1>Hi, I'm in a Portal</h1>}</Portal>
+              </>
+            )}
+          </Toggle> */}
+          <Toggle>
+            {({ on, toggle }) => (
+              <>
+                <button onClick={toggle}>Login</button>
+                <Modal on={on} toggle={toggle}>
+                  <h1>Hi there, I'm in a Modal</h1>
+                </Modal>
+              </>
             )}
           </Toggle>
         </header>
